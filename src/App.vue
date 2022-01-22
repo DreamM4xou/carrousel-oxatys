ece382f9a55334cdfdea809f49ba3c2c136d25c5
Max <maxime.torregiani@gmail.com>
2022-01-23 00:32:52 +0100

    first commit : deps + first steps component

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="app-test">
+    <div class="container-fluid">
+        <div class="card col-xs-12">
+            <div class="card-body">
+                <h3 class="card-title page-title">{{title}}</h3>
+                <div class="card-text">
+                    <carrousel
+                        :can_configure="is_admin"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import Carrousel from './components/Carrousel.vue'
+
+export default {
+    name: 'App',
+    components: {
+        'carrousel': Carrousel
+    },
+    data() {
+        return {
+            title: "Application Carrousel",
+            is_admin: true
+        };
+    }
+}
+</script>
---
+++ src/components/Carrousel.vue
@@ -0,0 +1,23 @@
+<template>
+    <button @click="count++">Tu as appuyé {{ count }} fois</button>
+    <div v-if="can_configure">Je suis admin</div>
+    <div v-else>Je ne suis pas admin</div>
+</template>
+
+<script>
+export default {
+    name: 'Carrousel',
+    props: {
+        canConfigure: {
+            type: Boolean,
+            required: false,
+            default() {return false;}
+        }
+    },
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
